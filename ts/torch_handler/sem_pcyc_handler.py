9d81343c0649f9f618a826c9a9fed23e803bf948
mwmw7 <cto@toonsquare.co>
2021-10-06 21:46:54 +0900

    Add baseHandler and sem_pcyc_handler.py

---
+++ ts/torch_handler/sem_pcyc_handler.py
@@ -0,0 +1,77 @@
+# custom handler file
+
+# model_handler.py
+
+"""
+ModelHandler defines a custom model handler.
+"""
+
+from ts.torch_handler.base_handler import BaseHandler
+
+class ModelHandler(BaseHandler):
+    """
+    A custom model handler implementation.
+    """
+
+    def __init__(self):
+        self._context = None
+        self.initialized = False
+        self.explain = False
+        self.target = 0
+
+    def initialize(self, context):
+        """
+        Initialize model. This will be called during model loading time
+        :param context: Initial context contains model server system properties.
+        :return:
+        """
+        self._context = context
+        print(self._context)
+        self.initialized = True
+        #  load the model, refer 'custom handler class' above for details
+
+    def preprocess(self, data):
+        """
+        Transform raw input into model input data.
+        :param batch: list of raw requests, should match batch size
+        :return: list of preprocessed model input data
+        """
+        # Take the input data and make it inference ready
+        preprocessed_data = data[0].get("data")
+        if preprocessed_data is None:
+            preprocessed_data = data[0].get("body")
+            print(preprocessed_data)
+        return preprocessed_data
+
+
+    def inference(self, model_input):
+        """
+        Internal inference methods
+        :param model_input: transformed model input data
+        :return: list of inference output in NDArray
+        """
+        # Do some inference call to engine here and return output
+        model_output = self.model.forward(model_input)
+        return model_output
+
+    def postprocess(self, inference_output):
+        """
+        Return inference result.
+        :param inference_output: list of inference output
+        :return: list of predict results
+        """
+        # Take output from network and post-process to desired format
+        postprocess_output = inference_output
+        return postprocess_output
+
+    def handle(self, data, context):
+        """
+        Invoke by TorchServe for prediction request.
+        Do pre-processing of data, prediction using model and postprocessing of prediciton output
+        :param data: Input data for prediction
+        :param context: Initial context contains model server system properties.
+        :return: prediction output
+        """
+        model_input = self.preprocess(data)
+        model_output = self.inference(model_input)
+        return self.postprocess(model_output)
