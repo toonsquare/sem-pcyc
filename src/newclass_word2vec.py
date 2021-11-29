a99db62b0e3279c0ee6eca2483b4d97b06159f5d
monkeykim111 <kwj102501@naver.com>
2021-11-29 19:21:41 +0900

    embedding the added words

---
+++ src/newclass_word2vec.py
@@ -0,0 +1,51 @@
+import glob
+import os
+import re
+
+import gensim
+import numpy as np
+
+import utils
+
+root_path = "/home/ubuntu/sem_pcyc/dataset/intersection/images"
+
+def create_wordemb(root_path, type='word2vec-google-news-300'):
+    clss = glob.glob(os.path.join(root_path, '*'))
+    clss = [c.split('/')[-1] for c in clss if os.path.isdir(c)]
+    available_type = ['word2vec-google-news-300', 'fasttext-wiki-news-subwords-300', 'glove-wiki-gigaword-300']
+    if type not in available_type:
+        print("Type specified does not exist.")
+        exit()
+    model = gensim.models.KeyedVectors.load_word2vec_format(
+        '/home/ubuntu/projects_paul/GoogleNews-vectors-negative300.bin', binary=True).wv
+
+    lv = len(model['airplane'])  # for knowing the length of vector
+
+    wordemb = dict()
+    # synonym dictionary
+    syn_dict = utils.get_synonym()
+    for cls in clss:
+        cls_tmp = re.sub('[)(]', '', cls)
+        if cls_tmp in model.vocab:
+            ws = [cls_tmp]
+        elif cls_tmp in syn_dict.keys():
+            ws = syn_dict[cls_tmp].split('_')
+        else:
+            ws = cls_tmp.split('_')
+        v = np.zeros((len(ws), lv))
+        for i, w in enumerate(ws):
+            if w in model.vocab:
+                v[i, :] = model[w]
+            else:
+                print(w)
+        wordemb.update({cls: np.mean(v, axis=0)})
+    return wordemb
+
+
+plus_word = {}
+
+plus_word = create_wordemb(root_path)
+
+# 생성한 dict를 npy로 만들기
+np.save('new_plus_words.npy', plus_word)
+print('npy create!')
