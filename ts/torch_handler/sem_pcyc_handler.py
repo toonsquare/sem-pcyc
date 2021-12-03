a949ce3426aa87670d7bfaeddfdaede7a54dc987
jonathan <kwj102501@naver.com>
2021-12-03 15:26:13 +0900

    update handler file path

--- ts/torch_handler/sem_pcyc_handler.py
+++ ts/torch_handler/sem_pcyc_handler.py
@@ -51,7 +51,10 @@ class ModelHandler(BaseHandler):
         self.acc_im_em = None
         self.acc_im_em_npy_saved = None
         self.max_prediction_size = 30
-        self.npy_path = '/home/model-server/npy'
+        # 모델 등록용
+        # self.npy_path = '/home/model-server/npy'
+        # 성능평가용
+        self.npy_path = '/home/ubuntu/projects_jonathan'
         npy_full_path = os.path.join(self.npy_path, "acc_im_em.npy")
         if os.path.isfile(npy_full_path):
             self.acc_im_em = self.np_load(npy_full_path)
@@ -166,8 +169,16 @@ class ModelHandler(BaseHandler):
         best_map = checkpoint['best_map']
         state_dict = checkpoint['state_dict']
         sem_dim = 0
+        '''
+        모델 등록용
         path_dataset = '/home/model-server/sem_pcyc/dataset'
         path_aux = '/home/model-server/sem_pcyc/aux'
+        self.dataset = dataset = 'intersection'
+        semantic_models = ['new_plus_words']
+        '''
+        # 검증용
+        path_dataset = '/home/ubuntu/sem_pcyc/dataset'
+        path_aux = '/home/ubuntu/sem_pcyc/aux'
         self.dataset = dataset = 'intersection'
         semantic_models = ['new_plus_words']
         files_semantic_labels = []
